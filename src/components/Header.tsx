4e17bf3ee852acf75bb108f657037a311247581c
onsbouguila2 <onsbouguila97@gmail.com>
2025-03-20 13:39:47 +0100

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-
 const Header = () => {
   return (
-    <header className="bg-gray-100 text-center py-4 shadow-md">
-      <h2 className="text-2xl font-bold text-gray-800">Neo Visualizer APP</h2>
+    <header className="bg-[#030432] text-center  flex justify-between items-center">
+            <img src="../public/Header-right-img.png" alt="Cover" className="w-35 h-auto " />
+      <div className="flex items-center gap-4 p-4">
+        <h2 className="text-4xl font-bold text-white ms-5">Neo Visualizer APP</h2>
+      </div>
+      <img src="../public/Header-left-img.png" alt="Cover" className="w-70 h-auto " />
     </header>
   );
 };
